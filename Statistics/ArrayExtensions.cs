24c27da86191dec81b3d5a0bc54056bdf92a681a
dv-lebedev <dv.lebedev777@gmail.com>
2015-12-11 17:38:54 +0300

    Add ArrayExtensions

---
+++ Statistics/ArrayExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    public static class ArrayExtensions
+    {
+        public static double[] ToDouble(this decimal[] array)
+        {
+            int size = array.Length;
+
+            double[] result = new double[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = (double)array[i];
+            }
+
+            return result;
+        }
+
+        public static decimal[] ToDecimal(this double[] array)
+        {
+            int size = array.Length;
+
+            decimal[] result = new decimal[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = (decimal)array[i];
+            }
+
+            return result;
+        }
+    }
+}
