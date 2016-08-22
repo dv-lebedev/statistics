c5223b8086e8a605967d3a6de7ae1eaa33876288
dv-lebedev <dv.lebedev777@gmail.com>
2016-08-22 19:08:12 +0300

    Remove region defs

--- Statistics/MovingAverages.cs
+++ Statistics/MovingAverages.cs
@@ -20,8 +20,6 @@ namespace Statistics
 {
     public static class MovingAverages
     {
-        #region IMPLEMENTATION OF METHODS FOR DOUBLE TYPE
-
         public static double[] SMA(double[] values, int period)
         {
             if (values == null) throw new ArgumentNullException("values");
@@ -29,6 +27,26 @@ namespace Statistics
             if (period <= 0) throw new ArgumentException("period <= 0");
 
             double[] result = new double[values.Length - (period - 1)];
+
+            for (int i = 0; i < values.Length - (period - 1); i++)
+            {
+                for (int j = i; j < i + period; j++)
+                {
+                    result[i] += values[j];
+                }
+                result[i] /= period;
+            }
+
+            return result;
+        }
+
+        public static decimal[] SMA(decimal[] values, int period)
+        {
+            if (values == null) throw new ArgumentNullException("values");
+            if (period > values.Length) throw new ArgumentException("period > values.Lenght");
+            if (period <= 0) throw new ArgumentException("period <= 0");
+
+            decimal[] result = new decimal[values.Length - (period - 1)];
 
             for (int i = 0; i < values.Length - (period - 1); i++)
             {
@@ -51,6 +69,31 @@ namespace Statistics
             long weight = 0;
             long weightSumm = 0;
             double[] result = new double[values.Length - (period - 1)];
+
+            for (int i = 0; i < values.Length - (period - 1); i++)
+            {
+                for (int j = i; j < i + period; j++)
+                {
+                    result[i] += values[j] * ++weight;
+                    weightSumm += weight;
+                }
+                result[i] /= weightSumm;
+                weight = 0;
+                weightSumm = 0;
+            }
+
+            return result;
+        }
+
+        public static decimal[] WMA(decimal[] values, int period)
+        {
+            if (values == null) throw new ArgumentNullException("values");
+            if (period > values.Length) throw new ArgumentException("period > values.Lenght");
+            if (period <= 0) throw new ArgumentException("period <= 0");
+
+            long weight = 0;
+            long weightSumm = 0;
+            decimal[] result = new decimal[values.Length - (period - 1)];
 
             for (int i = 0; i < values.Length - (period - 1); i++)
             {
@@ -92,55 +135,6 @@ namespace Statistics
             return result;
         }
 
-        #endregion
-
-        #region IMPLEMENTATION OF METHODS FOR DECIMAL TYPE
-
-        public static decimal[] SMA(decimal[] values, int period)
-        {
-            if (values == null) throw new ArgumentNullException("values");
-            if (period > values.Length) throw new ArgumentException("period > values.Lenght");
-            if (period <= 0) throw new ArgumentException("period <= 0");
-
-            decimal[] result = new decimal[values.Length - (period - 1)];
-
-            for (int i = 0; i < values.Length - (period - 1); i++)
-            {
-                for (int j = i; j < i + period; j++)
-                {
-                    result[i] += values[j];
-                }
-                result[i] /= period;
-            }
-
-            return result;
-        }
-
-        public static decimal[] WMA(decimal[] values, int period)
-        {
-            if (values == null) throw new ArgumentNullException("values");
-            if (period > values.Length) throw new ArgumentException("period > values.Lenght");
-            if (period <= 0) throw new ArgumentException("period <= 0");
-
-            long weight = 0;
-            long weightSumm = 0;
-            decimal[] result = new decimal[values.Length - (period - 1)];
-
-            for (int i = 0; i < values.Length - (period - 1); i++)
-            {
-                for (int j = i; j < i + period; j++)
-                {
-                    result[i] += values[j] * ++weight;
-                    weightSumm += weight;
-                }
-                result[i] /= weightSumm;
-                weight = 0;
-                weightSumm = 0;
-            }
-
-            return result;
-        }
-
         public static decimal[] VMA(decimal[] values, long[] volumes, int period)
         {
             if (values == null) throw new ArgumentNullException("values");
@@ -165,7 +159,5 @@ namespace Statistics
 
             return result;
         }
-
-        #endregion
     }
 }
